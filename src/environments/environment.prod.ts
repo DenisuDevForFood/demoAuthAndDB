71400e6ee157252cc157b6bacff1d4cbf0ee10bf
Denis Feier <denisfeier98@yahoo.com>
2020-03-16 09:35:35 +0200

    prep update to angular 9

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyCWIe73ChS9-GPWgq6P10SDAjljmcYLNaA',
-    authDomain: 'demofirestoreandauth.firebaseapp.com',
-    databaseURL: 'https://demofirestoreandauth.firebaseio.com',
-    projectId: 'demofirestoreandauth',
-    storageBucket: 'demofirestoreandauth.appspot.com',
-    messagingSenderId: '920476647898',
-    appId: '1:920476647898:web:e0e3f3c9f07a5c8625c0e3',
-    measurementId: 'G-QN8S9GSCM3'
+    apiKey: "AIzaSyCWIe73ChS9-GPWgq6P10SDAjljmcYLNaA",
+    authDomain: "demofirestoreandauth.firebaseapp.com",
+    databaseURL: "https://demofirestoreandauth.firebaseio.com",
+    projectId: "demofirestoreandauth",
+    storageBucket: "demofirestoreandauth.appspot.com",
+    messagingSenderId: "920476647898",
+    appId: "1:920476647898:web:61130955e45e2b5a25c0e3",
+    measurementId: "G-4VE1L6NKY0"
   }
 };
